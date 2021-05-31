509b462ec172e72879e780eee809fc8c31e4ad76
Edvinas Stanys <edvinasstanys@Edvinass-MacBook-Air.local>
2021-05-31 09:48:03 +0300

    ejs instaled, html to ejs, render pages.ejs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # js3-Blog-crud-express-ejs
+
+install ejs:
+npm install ejs
+and register ejs index js faile: app.set('view engine', 'ejs');
+pakeisti folder structure:
+pages pakeiciam i views ir visus .html failus i .ejs failus.
--- src/index.js
+++ src/index.js
@@ -4,19 +4,28 @@ const path = require('path');
 
 const PORT = 3000;
 
+// register view engine
+app.set('view engine', 'ejs');
+// nustatom render use home directorija
+app.set('views', 'src/views');
+
 //home page
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+  //   res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+  // paimti index.ejs faila is views direktorijos(render zinos kad ieskot view folderi)
+  res.render('index');
 });
 
 // about page
 app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+  //   res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+  res.render('about');
 });
 
 // blog page
 app.get('/blog', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pages', 'blog.html'));
+  //   res.sendFile(path.join(__dirname, 'pages', 'blog.html'));
+  res.render('blog');
 });
 
 const staticPath = path.join(__dirname, 'static');
